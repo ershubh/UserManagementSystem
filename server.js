c6fe4a6609dc6130a9e12955f3435aa4bfed8c45
BritoBrian <briankog23@gmail.com>
2023-07-18 22:31:20 +0100

    staticfilesdefined

--- server.js
+++ server.js
@@ -3,3 +3,13 @@ const app=express();
 const dotenv=require('dotenv');
 const bodyparser=require('body-parser');
 const path=require('path');
+
+app.use(bodyparser.urlencoded({extended:true}));
+
+app.use(morgan('tiny'));
+
+//Some static paths for css and javascript files
+app.use('/css',express.static(path.resolve(__dirname,'html/css')));
+app.use('/img',express.static(path.resolve(__dirname,'html/img')));
+app.use('/js',express.static(path.resolve(__dirname,'html/js')));
+

aad4998b8b86947af78358eea5c02c9100277722
BritoBrian <briankog23@gmail.com>
2023-07-18 22:52:53 +0100

    routercontrollercreated

--- server/routes/router.js
+++ server/routes/router.js
@@ -7,3 +7,14 @@ route.get('/',services.homeroutes);
 route.get('/add-user',services.add_user);
 // Route to connect to update page
 route.get('/update-user', services.update_user);
+
+// API post to create users
+route.post('/api/users',(req,res)=>{res.send('work')});
+// API post to find the users
+route.get('/api/users',controller.find);
+// API post to update the specified users
+route.put('/api/users/:id',controller.update);
+// API post to delete the specified users
+route.delete('/api/users/:id',controller.delet);
+
+module.exports=route;

96d3c7502627be8f247e0212602ee109e85866e7
anand <anandjayara@gmail.com>
2023-07-19 21:30:20 +0100

    render

--- server/services/render.js
+++ server/services/render.js
@@ -1,15 +1,7 @@
+const axios=require('axios');
 // create a service to render homepage
 const homeroutes=(req,res)=>{
-    res.render('index');
+    axios.get('http://localhost:8000/api/users').then((resp)=>{
+        console.log(resp.data);
+        res.render('index',{users:resp.data})}).catch(e=>res.send(e));
 }
-// create a service to render new_User page
-const add_user=(req,res)=>{
-    res.render('new_User');
-}
-// create a service to render update_User page
-const update_user=(req,res)=>{
-    res.render('update_user');
-}
-
-//export the module variables
-module.exports= {homeroutes, add_user, update_user};

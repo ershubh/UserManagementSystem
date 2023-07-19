408198b3fb467cded53e7190a35855292ecdcec4
anand <anandjayara@gmail.com>
2023-07-19 21:26:38 +0100

    createapi

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -1,2 +1,18 @@
 const { then } = require('../databases/connection.js');
 const userDb=require('../model/model.js');
+
+//to create the user
+const create=async (req,res)=>{
+    if(!req.body)
+    {
+     res.status(400).send('Content cannot be empty');
+     return;
+    }
+    const {name,email,gender,status}=req.body;
+    const user=userDb.create({
+     name,
+     email,
+     gender,
+     status,
+    }).then(data=>{ res.redirect('/add-user')}).catch(e=>console.log(e));
+ }

422c0a8b5b862896f83a9999923d40b3dcfee86c
anand <anandjayara@gmail.com>
2023-07-19 21:28:33 +0100

    filecompleted

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -3,20 +3,20 @@ const userDb=require('../model/model.js');
 
 //to create the user
 const create=async (req,res)=>{
-    if(!req.body)
-    {
-     res.status(400).send('Content cannot be empty');
-     return;
-    }
-    const {name,email,gender,status}=req.body;
-    const user=userDb.create({
-     name,
-     email,
-     gender,
-     status,
-    }).then(data=>{ res.redirect('/add-user')}).catch(e=>console.log(e));
- }
- //to find the specified user
+   if(!req.body)
+   {
+    res.status(400).send('Content cannot be empty');
+    return;
+   }
+   const {name,email,gender,status}=req.body;
+   const user=userDb.create({
+    name,
+    email,
+    gender,
+    status,
+   }).then(data=>{ res.redirect('/add-user')}).catch(e=>console.log(e));
+}
+//to find the specified user
 const find=(req,res)=>{
     if(req.query.id)
     {
@@ -40,22 +40,22 @@ const find=(req,res)=>{
 }
 //to update the specified user
 const update=(req,res)=>{
-    if(!req.body)
+ if(!req.body)
+ {
+    return res.status(500).send('Data cannot be empty');
+ } 
+const id=req.params.id;
+userDb.findByIdAndUpdate(id,req.body).then(data=>{
+    if(!data)
     {
-       return res.status(500).send('Data cannot be empty');
-    } 
-   const id=req.params.id;
-   userDb.findByIdAndUpdate(id,req.body).then(data=>{
-       if(!data)
-       {
-        res.send(`cannot update with ${id} May be user not found!`);
-       }
-       else
-       {
-           res.send(data);
-       }
-   }).catch(e=>res.status(500).send('error update user information'));}
-   //to delete the specified user
+     res.send(`cannot update with ${id} May be user not found!`);
+    }
+    else
+    {
+        res.send(data);
+    }
+}).catch(e=>res.status(500).send('error update user information'));}
+//to delete the specified user
 const delet=(req,res)=>{
     //Find the id from url 
     const id=req.params.id;
